3d29fb7f39baea72d66ffa11ae71e9971d8a73ce
Dexter Zhang <dejzhang@ucsc.edu>
2023-05-12 16:43:54 -0700

    Dexter Zhang, added some png's that I created and then I also added some basic movement for the player. Movement is not smooth

--- game.js
+++ game.js
@@ -1,22 +1,66 @@
-class Scene1 extends Phaser.Scene {
+class Intro extends Phaser.Scene {
     constructor() {
-        super('scene1');
+        super('intro');
     }
     create() {
-        this.add.text(50, 50, "That's all!").setFontSize(50);
-        this.add.text(50, 100, "Click anywhere to restart.").setFontSize(20);
-        this.input.on('pointerdown', () => this.scene.start('scene2'));
+        this.add.text(50, 50, "Intro Scene").setFontSize(50);
+        this.add.text(50, 100, "Click anywhere to start.").setFontSize(20);
+        this.input.on('pointerdown', () => this.scene.start('level1'));
     }
 }
 
-class Scene2 extends Phaser.Scene {
+class Level1 extends Phaser.Scene {
+    player;
+    bullet;
+    cursors;
+    speed;
+
+    constructor(){
+        super('level1');
+    }
+    preload ()
+    {
+        this.load.image('wizard', 'assets/pink pixel wizard.png');
+        this.load.image('bullet', 'assets/pixel projectile.png');
+    }
+    create(){
+        this.player = this.physics.add.image(256, 448, 'wizard');
+        this.cursors = this.input.keyboard.createCursorKeys();
+        // this.player = this.add.sprite(400, 500, 'wizard').setDepth(1);
+
+        this.speed = Phaser.Math.GetSpeed(300, 1);
+
+
+
+    }
+    update(time,delta){
+        if (this.cursors.left.isDown)
+        {
+            this.player.x -= this.speed * delta;
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.x += this.speed * delta;
+        }
+        else if (this.cursors.up.isDown)
+        {
+            this.player.y -= this.speed * delta;
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.player.y += this.speed * delta;
+        }
+    }
+}
+
+class Outro extends Phaser.Scene {
     constructor() {
-        super('scene2');
+        super('outro');
     }
     create() {
         this.add.text(50,50, "Scene 2 Hi").setFontSize(50);
         this.add.text(50, 100, "Click to go back to scene 1").setFontSize(20);
-        this.input.on('pointerdown', () => this.scene.start('scene1'));
+        this.input.on('pointerdown', () => this.scene.start('intro'));
     }
 }
 
@@ -27,6 +71,10 @@ const game = new Phaser.Game({
         width: 1920,
         height: 1080
     },
-    scene: [Scene1, Scene2],
+    physics: {
+        default: 'arcade',
+        arcade: { debug: true }
+    },
+    scene: [Intro, Level1, Outro],
     title: "Adventure Game",
 });
