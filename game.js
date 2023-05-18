a110895693968e1d1d080e5163993bd57fa47b9b
Dexter Zhang <dejzhang@ucsc.edu>
2023-05-18 09:38:07 -0700

    Dexter Zhang trying to figure out why I can't create new text after two obj touch

--- game.js
+++ game.js
@@ -20,6 +20,8 @@ class Level1 extends Phaser.Scene {
     keyD;
     keyW;
     boulder;
+    inZone = false;
+    temp = 0;
 
     constructor(){
         super('level1');
@@ -34,59 +36,29 @@ class Level1 extends Phaser.Scene {
         this.load.image('square', 'block.png');
         
     }
-    // create(){
-    //     // let keyA;
-    //     // let keyS;
-    //     // let keyD;
-    //     // let keyW;
-        
-
-    //     this.player = this.physics.add.sprite(256, 448, 'wizard').setBounce(0.2).setCollideWorldBounds(true);
-    //     this.boulder = this.physics.add.sprite(500, 450, 'square').setBounce(0.2).setCollideWorldBounds(true);
-
-        
-    //     this.cursors = this.input.keyboard.createCursorKeys();
-
-    //     this.physics.add.collider(this.player, this.boulder);   
-    //     this.physics.add.collider(this.boulder, this.player);
-
-    //     this.speed = Phaser.Math.GetSpeed(300, 1);
-
-    //     this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-    //     this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-    //     this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-    //     this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-    // }
-    // update(time, delta){
-
-    //     if(this.keyW.isDown || this.cursors.up.isDown) {
-    //         console.log('W key pressed');
-    //         this.player.y -= this.speed * delta;
-    //     }
-    //     if(this.keyA.isDown || this.cursors.left.isDown) {
-    //         console.log('A key pressed');
-    //         this.player.x -= this.speed * delta;
-    //     }
-    //     if(this.keyS.isDown || this.cursors.down.isDown) {
-    //         console.log('S key pressed');
-    //         this.player.y += this.speed * delta;
-    //     }
-    //     if(this.keyD.isDown || this.cursors.right.isDown) {
-    //         console.log('D key pressed');
-    //         this.player.x += this.speed * delta;
-    //     }
-    // }
 
     create ()
     {
 
 
         this.player = this.physics.add.sprite(100, 450, 'wizard').setBounce(0.2).setCollideWorldBounds(true);
-        this.boulder = this.physics.add.sprite(500, 450, 'square').setBounce(0.0).setCollideWorldBounds(true);
+        this.boulder = this.physics.add.sprite(500, 450, 'square').setBounce(0.2).setCollideWorldBounds(true);
 
         this.cursors = this.input.keyboard.createCursorKeys();
+        this.add.text(50, 50, "You can Move Boulders around by pushing them! Give it a Try.").setFontSize(50);
+        this.add.text(50, 100, "Use WASD or Arrow Keys to move around.").setFontSize(50);
+        console.log(this.inZone);
+        this.physics.add.collider(this.player, this.boulder, function(){
+            console.log("Player Touched the Boulder!")
+            // console.log(this);
+            this.temp = 1;
+            this.inZone = true;
+            // console.log(this.inZone);
+            
+            
+        });
 
-        this.physics.add.collider(this.player, this.boulder);
+        
 
         this.speed = Phaser.Math.GetSpeed(300, 1);
 
@@ -94,6 +66,114 @@ class Level1 extends Phaser.Scene {
         this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        
+        // this.physics.add.overlap(this.player, this.boulder, function () {
+        //     this.inZone = true;
+        //     console.log("Player Touched the Boulder!")
+        // });
+
+        // if(this.inZone){
+        //     console.log("Player Touched the Boulder!")
+        // }
+
+    }
+
+    update (time, delta)
+    {   
+        console.log(this.inZone);
+        console.log(this.temp);
+        if(this.inZone){
+            this.add.text(50, 150, "Click with mouse to move onto the next level.").setFontSize(20);
+            this.input.on('pointerdown', () => this.scene.start('level2'));
+        }
+
+        if(!this.cursors.left.isDown){
+            this.player.setVelocityX(0);
+            this.player.setVelocityY(0);
+        }
+        if (this.cursors.left.isDown || this.keyA.isDown)
+        {
+            this.player.setVelocityX(-300);
+
+        }
+        if (this.cursors.right.isDown || this.keyD.isDown)
+        {
+            this.player.setVelocityX(300);
+
+        }
+        if (this.cursors.up.isDown || this.keyW.isDown)
+        {
+            this.player.setVelocityY(-300);
+
+        }
+        if (this.cursors.down.isDown || this.keyS.isDown)
+        {
+            this.player.setVelocityY(300);
+
+        }
+    }
+
+}
+
+class Level2 extends Phaser.Scene {
+    player;
+    bullet;
+    cursors;
+    speed;
+    keyA;
+    keyS;
+    keyD;
+    keyW;
+    boulder;
+    inZone = false;
+
+    constructor(){
+        super('level2');
+       
+    }
+    preload ()
+    {
+        this.load.setPath('assets/');
+
+        this.load.image('wizard', 'pink pixel wizard.png');
+        this.load.image('bullet', 'pixel projectile.png');
+        this.load.image('square', 'block.png');
+        
+    }
+
+    create ()
+    {
+
+
+        this.player = this.physics.add.sprite(100, 450, 'wizard').setBounce(0.2).setCollideWorldBounds(true);
+        this.boulder = this.physics.add.sprite(500, 450, 'square').setBounce(0.2).setCollideWorldBounds(true);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.add.text(50, 50, "You can Move Boulders around by pushing them! Give it a Try.").setFontSize(50);
+        this.add.text(50, 100, "Use WASD or Arrow Keys to move around.").setFontSize(50);
+
+        this.physics.add.collider(this.player, this.boulder, function(){
+            console.log("Player Touched the Boulder!")
+            
+            this.add.text(50, 150, "Click with mouse to move onto the next level.").setFontSize(20);
+            this.input.on('pointerdown', () => this.scene.start('level2'));
+        });
+
+        this.speed = Phaser.Math.GetSpeed(300, 1);
+
+        this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        
+        // this.physics.add.overlap(this.player, this.boulder, function () {
+        //     this.inZone = true;
+        //     console.log("Player Touched the Boulder!")
+        // });
+
+        // if(this.inZone){
+        //     console.log("Player Touched the Boulder!")
+        // }
 
     }
 
@@ -105,28 +185,27 @@ class Level1 extends Phaser.Scene {
         }
         if (this.cursors.left.isDown || this.keyA.isDown)
         {
-            this.player.setVelocityX(-160);
+            this.player.setVelocityX(-300);
 
         }
-        if (this.cursors.right.isDown)
+        if (this.cursors.right.isDown || this.keyD.isDown)
         {
-            this.player.setVelocityX(160);
+            this.player.setVelocityX(300);
 
         }
-        if (this.cursors.up.isDown)
+        if (this.cursors.up.isDown || this.keyW.isDown)
         {
-            this.player.setVelocityY(-160);
+            this.player.setVelocityY(-300);
 
         }
-        if (this.cursors.down.isDown)
+        if (this.cursors.down.isDown || this.keyS.isDown)
         {
-            this.player.setVelocityY(160);
+            this.player.setVelocityY(300);
 
         }
     }
 
 }
-
 class Outro extends Phaser.Scene {
     constructor() {
         super('outro');
@@ -149,6 +228,6 @@ const game = new Phaser.Game({
         default: 'arcade',
         arcade: { debug: true }
     },
-    scene: [Intro, Level1, Outro],
+    scene: [Intro, Level1, Level2, Outro],
     title: "Adventure Game",
 });
